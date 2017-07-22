6881681b4e94832e7877cb189516de1ca7b5b00d
Jon Lindell <jhlindell@yahoo.com>
2017-07-22 17:03:28 -0600

    basic routing introduced

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 import Property from './components/property.js';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
   state = {
     property: {
-          owner_id: 1,
-          property_name: 'Haunted Hummus House',
-          street_address: '1789 Nutscaping View',
-          city: 'Boulder',
-          state: 'CO',
-          zip_code: 80301,
-          photo_url: '',
-          description: 'This place is haunted as fuck. The ghosts here will scare the falafel out of you. Book here if you got the stones.',
-          amenities: 'running water and power',
-          nightly_price: 29.99,
-          house_rules: 'Keep the noise down.'
-        }
+      owner_id: 1,
+      property_name: 'Haunted Hummus House',
+      street_address: '1789 Nutscaping View',
+      city: 'Boulder',
+      state: 'CO',
+      zip_code: 80301,
+      photo_url: '',
+      description: 'This place is haunted as fuck. The ghosts here will scare the falafel out of you. Book here if you got the stones.',
+      amenities: 'running water and power',
+      nightly_price: 29.99,
+      house_rules: 'Keep the noise down.'
+    }
   }
   render() {
     return (
-      <div>
-        <h1>Whassssssup World?</h1>
-        <Property property={this.state.property}/>
-      </div>
+      <Router>
+        <div>
+          <h1>Nav Links:</h1>
+          <Link to="/">Home</Link>
+          <Route path="/property/" component={() => <Property property={this.state.property} />} />
+        </div>
+      </Router>
     )
   }
 }
