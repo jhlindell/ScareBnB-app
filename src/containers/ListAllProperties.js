6360a47f49d6305f3770ecfacb039ae184cba6c8
Jon Lindell <jhlindell@yahoo.com>
2017-07-26 17:49:23 -0600

    pulled out listAll filter to use later

--- src/containers/ListAllProperties.js
+++ src/containers/ListAllProperties.js
@@ -1,61 +1,51 @@
 import React from "react";
-import { Container, Input, Label,Row, Col, Form, FormGroup } from 'reactstrap';
+import {
+  Container,
+  Input,
+  Label,
+  Row,
+  Col,
+  Form,
+  FormGroup
+} from 'reactstrap';
 import PropertyCard from '../components/propertyCard';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {selectProperty} from '../actions/index';
 import {getAllProperties} from '../actions/index';
 
-
 class ListAllProperties extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleInputChange(event){
+  handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    this.setState({
-      [name]: value
-    });
+    this.setState({[name]: value});
   }
 
   componentDidMount() {
     this.props.getAllProperties();
   }
 
-  renderList(){
-    if(!this.props.properties){
+  renderList() {
+    if (!this.props.properties) {
       return <div>loading</div>
     }
-    return this.props.properties.map((property, i)=>{
-      return (
-        <PropertyCard
-          key={i}
-          selectProperty={this.props.selectProperty}
-          property={property}
-        />
-      )
+    return this.props.properties.map((property, i) => {
+      return (<PropertyCard key={i} selectProperty={this.props.selectProperty} property={property}/>)
     });
   }
 
-  render(){
+  render() {
     return (
 
       <Container-fluid className="ListAllProperties">
-          <Row className="cardHolder">
-            <div className="filterBox">
-              <Form>
-                 <FormGroup>
-                   <Label for="exampleEmail">Email</Label>
-                   <Input type="email" name="email" id="exampleEmail" onChange={(e) => {this.handleInputChange(e)}} placeholder="with a placeholder" />
-                 </FormGroup>
-              </Form>
-            </div>
-          <Col xs='2'>
-          </Col>
+        <Row className="cardHolder">
+          <Col xs='2'></Col>
           <Col xs='9'>
             <Row>
               {this.renderList()}
@@ -63,20 +53,31 @@ class ListAllProperties extends React.Component {
           </Col>
         </Row>
       </Container-fluid>
-
     )
   }
 }
 
 function mapStateToProps(state) {
-  return {
-    properties: state.properties
-  };
+  return {properties: state.properties};
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators(
-    {selectProperty: selectProperty, getAllProperties: getAllProperties}, dispatch);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    selectProperty: selectProperty,
+    getAllProperties: getAllProperties
+  }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListAllProperties);
+
+//we'll use this later
+{/* <div className="filterBox">
+  <Form>
+    <FormGroup>
+      <Label for="exampleEmail">Email</Label>
+      <Input type="email" name="email" id="exampleEmail" onChange={(e) => {
+        this.handleInputChange(e)
+      }} placeholder="with a placeholder"/>
+    </FormGroup>
+  </Form>
+</div> */}
