9db07e691865b8896bb6569988f88d9b691d40cf
Sol Laudon <blaudon1@gmail.com>
2017-07-26 14:34:12 -0600

    add reserved! tag to property cards if reserved

--- src/components/propertyCard.js
+++ src/components/propertyCard.js
@@ -12,6 +12,7 @@ class PropertyCard extends React.Component {
             {this.props.property.property_name}
           </CardTitle>
         </CardBlock>
+        <div className={`${this.props.property.bookedOnHalloween?  'reservedStamp' : 'none'}`}>Reserved!</div>
         <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="a something should go here" />
         <CardBlock className="overflow" >
           <CardText >
