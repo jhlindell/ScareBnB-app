cca89271a97372eae59407ad388c571861e15195
Jordan Klaers <jordan.klaers@gmail.com>
2017-07-27 15:58:39 -0600

    created splash page, email also included

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,20 +9,21 @@ class LandingPage extends React.Component {
       <div>
 
           <div className="firstSentnace">
-              <h5 className="fadeSentance">The only thing scarier than visiting a haunted house is  <span id="living">living</span>  in a haunted house</h5>
-              <h5 id="dying"> dying </h5>
+              <h5 className="fadeSentance">The only thing scarier than visiting a haunted house is  <span id="living">living</span> <span id="dying"> dying </span> in a haunted house</h5>
+
           </div>
           <div className="secondSentance">
-            <h5>This Halloween, ScareBNB is offering an authentic terror experience with an overnight stay in peer-hosted haunted houses </h5>
+            This Halloween, ScareBNB is offering an authentic terror experience with an overnight stay in peer-hosted haunted houses
           </div>
-          
+
 
 
 
           <div className="menubtncontainer">
             <div className="bar"></div>
             <div className="dripwrapper">
-              <div id="bloodtext"> Enter at your own risk</div>
+              <div><Link to="/bg/listallproperties" className="bloodtext">Enter at your own risk</Link> </div>
+
               <div className="dripper"></div>
               <div className="drop"></div>
             </div>
