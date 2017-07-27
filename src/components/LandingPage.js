6c6ffc25a5094f74e1d8088af275e8fd1d0b8820
Jordan Klaers <jordan.klaers@gmail.com>
2017-07-27 14:56:13 -0600

    text inside button, dynamic, and text on page, not dynamic

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,16 +6,29 @@ import {Link} from 'react-router-dom'
 class LandingPage extends React.Component {
   render(){
     return (
+      <div>
+
+          <div className="firstSentnace">
+              <h5 className="fadeSentance">The only thing scarier than visiting a haunted house is  <span id="living">living</span>  in a haunted house</h5>
+              <h5 id="dying"> dying </h5>
+          </div>
+          <div className="secondSentance">
+            <h5>This Halloween, ScareBNB is offering an authentic terror experience with an overnight stay in peer-hosted haunted houses </h5>
+          </div>
+          
+
+
 
           <div className="menubtncontainer">
             <div className="bar"></div>
             <div className="dripwrapper">
-
+              <div id="bloodtext"> Enter at your own risk</div>
               <div className="dripper"></div>
               <div className="drop"></div>
             </div>
           </div>
 
+</div>
     );
   }
 }
