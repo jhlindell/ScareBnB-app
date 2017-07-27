4fc0a472c6205ad4b81d99eeb63e3c3283cf5523
Sol Laudon <blaudon1@gmail.com>
2017-07-27 10:32:50 -0600

    add landing page and rerouted nav, background, and all other pages to work

---
+++ src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from 'reactstrap';
+import {Link} from 'react-router-dom'
+
+
+class LandingPage extends React.Component {
+  render(){
+    return (
+      <Container-fluid className="landingPageContainer">
+        <p>Hello</p>
+      </Container-fluid>
+    );
+  }
+}
+
+
+export default LandingPage;
